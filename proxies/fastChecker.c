347918f0a2eb71f4ddbdd3323e4e978cd9a5e63d
guifre <guifre.ruiz@owasp.org>
2015-02-24 17:57:38 -0500

    minor code cleanup

--- proxies/fastChecker.c
+++ proxies/fastChecker.c
@@ -7,12 +7,11 @@
 #include <pthread.h>
 
 int create_tcp_socket();
-char *get_ip(char *host);
 char *build_get_query(char *host);
 void *senda(void *hos);
 
 /* proxy validator
-usage $ gcc fastChecker.c -o fastChecker; ./fastChecker proxy.list
+usage $ gcc fastChecker.c -o fastChecker -lpthread; ./fastChecker proxy.list
 */
 int main(int argc, char **argv) {
 	char * target;
@@ -56,40 +55,34 @@ void *senda(void *hos)
 
 	if (port == NULL || host == NULL || strlen(port) < 2 || strlen(host) <4)
 	{
-		printf("something null, quitting");
-    	return -1;
+    		return;
 	}
-	printf("host[%s] port[%s]\n", host, port);
+	// printf("checking host[%s] port[%s]\n", host, port);
 	struct sockaddr_in *remote;
 	int sock;
 	int tmpres;
-	char *ip;
 	char *get;
 	char buf[BUFSIZ+1];
 
 	sock = create_tcp_socket();
-	//ip = get_ip(host);
-	//fprintf(stderr, "IP is %s\n", ip);
 	remote = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in *));
 	remote->sin_family = AF_INET;
 	tmpres = inet_pton(AF_INET, host, (void *)(&(remote->sin_addr.s_addr)));
 	if( tmpres < 0)
 	{
-  		perror("Can't set remote->sin_addr.s_addr");
-		exit(1);
+  		//perror("Can't set remote->sin_addr.s_addr");
 	}
-	else if(tmpres == -1)
+	else if (tmpres == -1)
 	{
-  		fprintf(stderr, "%s is not a valid IP address\n", ip);
+  		//fprintf(stderr, "%s is not a valid IP address\n", host);
 	}
 	remote->sin_port = htons(atoi(port));
-
-	if(connect(sock, (struct sockaddr *)remote, sizeof(struct sockaddr)) < 0)
+	if (connect(sock, (struct sockaddr *)remote, sizeof(struct sockaddr)) < 0)
 	{
-		perror("Could not connect");
+		//perror("Could not connect"); 
+		return;
 	}
 	get = build_get_query(host);
-	fprintf(stderr, "Query is:\n<<START>>\n%s<<END>>\n", get);
 
 	//Send the query to the server
 	int sent = 0;
@@ -98,20 +91,20 @@ void *senda(void *hos)
 		tmpres = send(sock, get+sent, strlen(get)-sent, 0);
 		if(tmpres == -1)
 		{
-			// perror("Can't send query");
+			 perror("Can't send query");
 		}
 	 	sent += tmpres;
 	}
 	//now it is time to receive the page
 	memset(buf, 0, sizeof(buf));
 	int htmlstart = 0;
-	char * htmlcontent;
-	while((tmpres = recv(sock, buf, BUFSIZ, 0)) > 0)
+	char *htmlcontent;
+	while ((tmpres = recv(sock, buf, BUFSIZ, 0)) > 0)
 	{
-		if(htmlstart == 0)
+		if (htmlstart == 0)
   		{
 			htmlcontent = strstr(buf, "\r\n\r\n");
-			if(htmlcontent != NULL)
+			if (htmlcontent != NULL)
 			{
 				htmlstart = 1;
 				htmlcontent += 4;
@@ -121,22 +114,27 @@ void *senda(void *hos)
 		{
   			htmlcontent = buf;
 		}
-		if(htmlstart)
+		if (htmlstart)
   		{
-			fprintf(stdout, htmlcontent);
+			// fprintf(stdout, htmlcontent);
+			if (strstr(htmlcontent, "Google is") != NULL)
+			{
+				printf("%s:%s is valid\n", host, port);
+			}
   		}
-		printf("got %s", htmlcontent);
 		memset(buf, 0, tmpres);
 	}
-	if(tmpres < 0)
+	if (tmpres < 0)
 	{
 	  perror("Error receiving data");
 	}
+    if (strstr(htmlcontent, "Google is built by a large team of engineers") != NULL)
+    {
+        printf("%s is valid\n", host);
+    }
 	free(get);
 	free(remote);
-	free(ip);
 	close(sock);
-	printf("exiting");
 }
 
 
@@ -152,33 +150,13 @@ int create_tcp_socket()
 }
 
 
-char *get_ip(char *host)
-{
-	struct hostent *hent;
-	int iplen = 15; //XXX.XXX.XXX.XXX
-	char *ip = (char *)malloc(iplen+1);
-	memset(ip, 0, iplen+1);
-	if((hent = gethostbyname(host)) == NULL)
-	{
-  		herror("Can't get IP");
-		exit(1);
-	}
-	if(inet_ntop(AF_INET, (void *)hent->h_addr_list[0], ip, iplen) == NULL)
-	{
-  		perror("Can't resolve host");
- 		exit(1);
-	}
-
-	return ip;
-}
-
 char *build_get_query(char *host)
 {
 	char *query;
 	char *req;
 	req = (char *) malloc ( 1024 + 1 );
 	strcpy(req, "http://www.google.com/humans.txt");
-	char *tpl = "GET http://www.google.com/humans.txt HTTP/1.0\r\n\r\n";
+	char *tpl = "GET http://www.google.com/humans.txt HTTP/1.1\r\nAccept: */*\r\nMozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36\r\n\r\n";
 	query = (char *)malloc(strlen(host)+strlen(req)+strlen(tpl)-5);
 	sprintf(query, tpl,  host);
 
